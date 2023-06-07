4b0b4bdc296ba60df59cb8db2bf4f84c379569ee
nurzigit-mg <turdumamatovnurzhigit@gmail.com>
2023-06-07 13:13:05 +0600

    header

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Route, Routes} from "react-router-dom";
 import Home from "./Page/Home";
+import Login from "./components/Header/Login";
 
 function App() {
     return (
         <div className="App">
-            <Header/>
-            <Routes>
-                <Route path={"/"} element={<Home/>}/>
-                {/*<Route path={"/"} element={<Home/>}/>*/}
-                {/*<Route path={"/"} element={<Home/>}/>*/}
-            </Routes>
-            <Footer/>
+                <Header/>
+                <Routes>
+                    <Route path={"/"} element={<Home/>}/>
+                    <Route path={"/aboutUs"} element={<Home/>}/>
+                    {/*<Route path={"/"} element={<Home/>}/>*/}
+                    <Route path={"/login"} element={<Login/>}/>
+                </Routes>
+                <Footer/>
+
         </div>
     );
 }
