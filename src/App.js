a644eac0c15d322218495e512c719af5c4ac63b7
TheAitbay <113280367+aitbek21v@users.noreply.github.com>
2023-06-12 11:53:22 +0600

    about'finished

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+import React from "react";
 import './App.scss';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Route, Routes} from "react-router-dom";
 import Home from "./Page/Home";
 import Login from "./components/Header/Login";
+import About from "./components/About";
+import AboutUs from "./Page/ AboutUs";
 
 function App() {
     return (
         <div className="App">
                 <Header/>
                 <Routes>
-                    <Route path={"/"} element={<Home/>}/>
+                    <Route path={"/"} element={<About/>}/>
                     <Route path={"/aboutUs"} element={<Home/>}/>
                     {/*<Route path={"/"} element={<Home/>}/>*/}
                     <Route path={"/login"} element={<Login/>}/>
+                    <Route path={"/all"} element={<AboutUs/>}/>
                 </Routes>
-                <Footer/>
+            <Footer/>
 
         </div>
     );
